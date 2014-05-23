2a8a4adbe9d9ae55e85e871c8f0410e007c18433
lacti <lactrious@gmail.com>
2014-05-24 00:36:14 +0900

    add bin/gen to gitignore

--- gen/net/daum/android/map/openapi/sampleapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.daum.android.map.openapi.sampleapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
